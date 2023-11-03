80c8f67abb5235fe2d831971e902df8f4d541962
Amine shx <98875453+amineshx@users.noreply.github.com>
2023-11-03 15:20:30 +0100

    Update data.js

--- src/components/ProjectNameSpace/data.js
+++ src/components/ProjectNameSpace/data.js
@@ -1,5 +1,7 @@
 export default [
     {
-        progress: 97
+        progress: 100,
+        title:"Project’s name space",
+        discription:"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,consectetur, adipisci velit..., There is no one who loves pain itself,who seeks after it and wants to have it, simply because it is pain."
     }
 ]
