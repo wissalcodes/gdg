08ffca6d2e6ba818e8b4ee06f18ca151ea616ce7
wissalcodes <lw_messikh@esi.dz>
2023-11-07 00:55:43 +0100

    ..

--- src/components/CheckPoints/CheckpointsForm.jsx
+++ src/components/CheckPoints/CheckpointsForm.jsx
@@ -1,10 +1,8 @@
 import DeadlineCard from "./DeadlineCard";
 
 const CheckpointsForm = () => {
-
-
   return (
-    <div className="absolute top-[154px] left-[1294px] rounded-2xl bg-whitesmoke w-[584px] h-[877px] overflow-hidden text-left text-5xl text-black font-product-sans ">
+    <div className="absolute top-[154px] left-[1294px] rounded-2xl bg-whitesmoke w-full h-[877px] overflow-hidden text-left text-5xl text-black font-product-sans ">
       <div className="absolute top-[32px] left-[32px] font-product-sans-medium ">
         Checkpoints
       </div>
@@ -13,17 +11,9 @@ const CheckpointsForm = () => {
         className="absolute top-[27px] left-[506px] w-9 h-[40.9px] cursor-pointer"
         alt=""
         src="/outlineinterfaceplus2.svg"
-
       />
-      <DeadlineCard
-        checkpointLabel="Checkpoint 1"
-
-      />
-      <DeadlineCard
-        checkpointLabel="Checkpoint 2"
-        propTop="509px"
-
-      />
+      <DeadlineCard checkpointLabel="Checkpoint 1" />
+      <DeadlineCard checkpointLabel="Checkpoint 2" propTop="509px" />
       <img
         className="absolute top-[150.3px] left-[549.5px] w-6 h-[666.7px]"
         alt=""
